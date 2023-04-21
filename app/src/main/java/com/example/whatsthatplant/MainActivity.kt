b5e1c1f463e2ba4737b7302e7b95d16a56147711
Clay Crews <claycrews2002@gmail.com>
2023-04-21 16:11:43 -0400

    fix: add glide module

--- app/src/main/java/com/example/whatsthatplant/MainActivity.kt
+++ app/src/main/java/com/example/whatsthatplant/MainActivity.kt
@@ -158,7 +158,6 @@ class MainActivity : AppCompatActivity() {
                 }
 
 
-
                 Glide.with(this)
                     .load(imageUrl)
                     .placeholder(placeholderImg)
